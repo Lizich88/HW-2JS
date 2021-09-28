e387dbdb3f5f5efb756454e2c7d3d325e2d49dda
AalamTeam <87983971+Lizich88@users.noreply.github.com>
2021-09-28 20:33:37 +0200

    HW2-JS

---
+++ js/script.js
@@ -0,0 +1,5 @@
+document.write("Shadybekova Elzada" + "<br>" + "elzada.shadybekova.kk@gmail.com" + "<br>");
+document.write("Gustaaf Callierlaan #244" + "<br>" + "9000 Gent Belgium")
+
+let userName = prompt("What's your name?");
+alert("Hello" + userName);
